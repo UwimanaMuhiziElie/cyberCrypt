b8e525be3563dde64292e8ab6b8c6525ee494910
Elie Uwimana <muhizielie01@gmail.com>
2024-04-08 19:42:22 +0200

    removed comments in hasher.py

--- cybercrypt/hasher.py
+++ cybercrypt/hasher.py
@@ -1,36 +1,15 @@
-# import hashlib
-# import bcrypt
-
-
-# HASH_ALGORITHMS = set(hashlib.algorithms_guaranteed) | {'sha3_256', 'sha512', 'bcrypt'}
-
-# def hash_data(data, hash_algorithm):
-#     if hash_algorithm == 'bcrypt':
-#         return bcrypt_hash(data)
-#     hasher = hashlib.new(hash_algorithm)
-#     hasher.update(data.encode())
-#     return hasher.hexdigest()
-
-# def bcrypt_hash(data):
-#     salt = bcrypt.gensalt()
-#     hashed_data = bcrypt.hashpw(data.encode(), salt)
-#     return hashed_data.decode()
-
-
 import hashlib
 import bcrypt
 
 HASH_ALGORITHMS = set(hashlib.algorithms_guaranteed) | {'sha3_256', 'sha512', 'bcrypt'}
 
+
 def hash_data(data, hash_algorithm):
     if hash_algorithm == 'bcrypt':
         return bcrypt_hash(data)
-    elif hash_algorithm in HASH_ALGORITHMS:
-        hasher = hashlib.new(hash_algorithm)
-        hasher.update(data.encode())
-        return hasher.hexdigest()
-    else:
-        raise ValueError("[!] Unsupported hash type.")
+    hasher = hashlib.new(hash_algorithm)
+    hasher.update(data.encode())
+    return hasher.hexdigest()
 
 def bcrypt_hash(data):
     salt = bcrypt.gensalt()
