9b50c90cc489c982c4b5cf0734af36d73a187355
Elie Uwimana <muhizielie01@gmail.com>
2024-04-08 19:38:55 +0200

    updated utilities functionality

--- cybercrypt/utils.py
+++ cybercrypt/utils.py
@@ -1,16 +1,62 @@
+import os
+import getpass
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
+def secure_store_key(key, filename, passphrase=None):
+    if passphrase:
+        salt = os.urandom(16)
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        key = kdf.derive(passphrase.encode())
+    with open(filename, 'wb') as file:
+        file.write(key)
+
+def secure_load_key(filename, passphrase=None):
+    with open(filename, 'rb') as file:
+        key = file.read()
+    if passphrase:
+        salt = key[:16]
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        key = kdf.derive(passphrase.encode())
+    return key
+
+def get_passphrase(prompt='Enter passphrase: '):
+    return getpass.getpass(prompt)
+
+def prompt_passphrase(prompt='Enter passphrase: '):
+    passphrase = get_passphrase(prompt)
+    confirm_passphrase = getpass.getpass('Confirm passphrase: ')
+    if passphrase != confirm_passphrase:
+        raise ValueError('Passphrases do not match.')
+    return passphrase
+
 def aes_encrypt(data, key):
-    iv = b'your_random_iv_16_bytes'
+    iv = os.urandom(16)
     cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     encryptor = cipher.encryptor()
     encrypted_data = encryptor.update(data) + encryptor.finalize()
-    return encrypted_data
+    return iv + encrypted_data
 
 def aes_decrypt(encrypted_data, key):
-    iv = b'your_random_iv_16_bytes'
+    iv = encrypted_data[:16]
+    encrypted_data = encrypted_data[16:]
     cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     decrypted_data = decryptor.update(encrypted_data) + decryptor.finalize()
     return decrypted_data
+
