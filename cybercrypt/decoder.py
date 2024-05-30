754ddda9ab6487c2ffc2fdec99850b4c4effeaa9
Elie Uwimana <muhizielie01@gmail.com>
2024-05-30 12:30:24 +0200

    fix: removed comments

--- cybercrypt/decoder.py
+++ cybercrypt/decoder.py
@@ -1,16 +1,4 @@
-# import base64
-# import urllib.parse
-# import html
 
-# DECODERS = {
-#     "base64": base64.b64decode,
-#     "hex": bytes.fromhex,
-#     "url": lambda data: urllib.parse.unquote(data),
-#     "html": html.unescape
-# }
-
-# def decode(data, decoding_type):
-#     return DECODERS[decoding_type](data)
 
 import base64
 import urllib.parse
