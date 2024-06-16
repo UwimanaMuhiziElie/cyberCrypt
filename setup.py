bba43feef33a60d0605da8a62a07a11a4e937cbf
Elie Uwimana <muhizielie01@gmail.com>
2024-06-16 18:41:29 +0200

    updated the version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cybercrypt',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(include=['core', 'core.*']),
     entry_points={
         'console_scripts': [
