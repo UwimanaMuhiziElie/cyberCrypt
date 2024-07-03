1c1f51904bc07bedc56a46922c9091a4d2922280
Elie Uwimana <muhizielie01@gmail.com>
2024-07-03 08:11:00 +0200

    update: added bcrypt installer

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     entry_points={
         'console_scripts': [
             'cyberCrypt=cybercrypt:main',
-            'cyberCrypt-windows=cybercrypt:main',  # For Windows
-            'cyberCrypt-linux=cybercrypt:main',    # For Linux
+            'cyberCrypt-windows=cybercrypt:main',  
+            'cyberCrypt-linux=cybercrypt:main',   
         ],
     },
     install_requires=[
@@ -16,7 +16,8 @@ setup(
         'colorama',
         'cryptography',
         'pytest-asyncio',  
-        'aiofiles',       
+        'aiofiles',
+        'bcyrpt',       
     ],
     extras_require={
         'dev': [
@@ -34,5 +35,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
